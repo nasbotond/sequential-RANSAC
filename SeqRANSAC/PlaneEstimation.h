2b3a32d8d90c23cf5ca0e4cc9e8dd398bc36e18a
nasbotond <nas.botond@gmail.com>
2021-11-08 13:18:05 +0100

    fix header file

--- SeqRANSAC/PlaneEstimation.h
+++ SeqRANSAC/PlaneEstimation.h
@@ -24,7 +24,6 @@ typedef struct RANSACDiffs
 } RANSACDiffs;
 
 
-float* EstimatePlaneImplicit(vector<Point3f>);
 float* EstimatePlaneOptimal(vector<Point3f>);
 float* EstimatePlaneRANSAC(vector<Point3f>,float,int);
 RANSACDiffs PlanePointRANSACDifferences(vector<Point3f> pts, float* plane, float threshold);
